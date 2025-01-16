4fdac9e8e0b325988081c64a79b67579b73f2316
louieeeee <louiesanqui0518@gmail.com>
2025-01-16 21:51:22 +0800

    last commit

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,12 +1,26 @@
 //import React from 'react'
 
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 const Users = () => {
-    const [users, setUsers] = useState([{
-        Name: "louie", Email: "louie@gmail.com", Age: 27
-    }])
+    const [users, setUsers] = useState([])
+
+    useEffect(()=> {
+        axios
+        .get('http://localhost:3001')
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = (id) => {
+        axios
+        .delete('http://localhost:3001/deleteUser/'+id)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
         <div className="w-50 bg-white rounded p-3">
@@ -25,12 +39,12 @@ const Users = () => {
                         users.map((user) => {
                             // eslint-disable-next-line react/jsx-key
                             return <tr>
-                                <td>{user.Name}</td>
-                                <td>{user.Email}</td>
-                                <td>{user.Age}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.age}</td>
                                 <td>
-                                <Link to="/update" className='btn btn-success'>Update</Link>
-                                <button className='btn btn-danger'>Delete</button>
+                                <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
+                                <button className='btn btn-danger' onClick={(e) => handleDelete(user._id)}>Delete</button>
                                 </td>
                             </tr>
                         })
